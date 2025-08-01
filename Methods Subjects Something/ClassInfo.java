824fc7b22f1f0ebf189a4aecc0e9fc8bb5f37452
SafiZ <tcabato@addu.edu.ph>
2025-08-01 22:14:44 +0800

    Doing too much
    wtf are you doing man

--- Methods Subjects Something/ClassInfo.java
+++ Methods Subjects Something/ClassInfo.java
@@ -4,24 +4,21 @@ import java.util.*;
 public class ClassInfo {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scan = new Scanner(new File("Students.txt"));
-        int totalStudents = scan.nextInt(); scan.nextLine();
-        Students allStudents[] = new Students[totalStudents];
-
+        int totalStudents = scan.nextInt(); 
+        scan.nextLine();
+        Students[] allStudents = new Students[totalStudents];
+    
         for (int i = 0; i < totalStudents; i++) {
-            int totalUnits = 0;
-            float average = 0;
-            int totalSubjects = scan.nextInt(); scan.nextLine();
             String name = scan.nextLine();
-            for (int j = 0; j < totalSubjects; j++) {
-                scan.nextLine(); // Course
-                int units = scan.nextInt();
-                int grade = scan.nextInt();
-                totalUnits += units;
-                average += grade * units;
-            } average /= totalUnits;
-            allStudents[i] = new Students(name, average);
-        }
-
+            int totalSubjects = scan.nextInt(); 
+            scan.nextLine();
+            
+            Students student = new Students(name, 0);
+            float average = student.takeSubjectAverage(scan, totalSubjects);
+            student.average = average;
+            allStudents[i] = student;
+        } scan.close();
+    
         for (Students student : allStudents) {
             student.displayInfo();
         }
--- Methods Subjects Something/Students.java
+++ Methods Subjects Something/Students.java
@@ -1,6 +1,32 @@
+import java.util.*;
+
 public class Students {
     String name;
     float average;
+
+    public void extraNewLines(Scanner scan) {
+        if (!scan.hasNextInt() || !scan.hasNextFloat()) {
+            scan.nextLine();
+        }
+    }
+
+    public float takeSubjectAverage(Scanner scan, int totalSubjects) {
+        float totalUnits = 0, totalGradePoints = 0;
+        
+        for (int i = 0; i < totalSubjects; i++) {
+            String subjectLine = scan.nextLine();
+            // Supposedly subject to be stored in an array but idk what to do with it
+            extraNewLines(scan);
+            int units = scan.nextInt();
+            extraNewLines(scan);
+            float grade = scan.nextFloat();
+            
+            if (scan.hasNextLine()) scan.nextLine();
+            
+            totalUnits += units;
+            totalGradePoints += (grade * units);
+        } return totalGradePoints / totalUnits;
+    }
 
     public Students(String name, float average) {
         this.name = name;
